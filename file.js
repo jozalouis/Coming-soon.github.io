133189f8ccffb01ed2da65a8dacbc5459d326133
Josephina <josephinalouisgit config --global user.email josephinalouis@gmail.com>
2021-01-25 18:48:29 -0800

    Coming soon page

---
+++ file.js
@@ -0,0 +1,27 @@
+var comingdate = new Date("jan 1, 2022 00:00:00");
+
+var d = document.getElementById('d');
+var h = document.getElementById('h');
+var m = document.getElementById('m');
+var s = document.getElementById('s');
+
+var x = setInterval(function(){
+
+    var now = new Date();
+    var des = comingdate.getTime() - now.getTime();
+    var days = Math.floor(des/(1000 * 60 * 60 * 24));
+    var hours = Math.floor(des%(1000 * 60 * 60 * 24) / (1000 * 60 * 60));
+    var mins = Math.floor(des%(1000 * 60 * 60) / (1000 * 60));
+    var secs = Math.floor(des%(1000 * 60) / 1000);
+
+    d.innerHTML = getTruenumber(days);
+    h.innerHTML = getTruenumber(hours);
+    m.innerHTML = getTruenumber(mins);
+    s.innerHTML = getTruenumber(secs);
+
+},1000);
+
+function getTruenumber(x){
+    if(x<10) return '0'+x;
+    else return x;
+};
